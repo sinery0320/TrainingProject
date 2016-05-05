eddcb316d6499e2847abd0fa09c66ddd9e8aeefa
Nicholas Wang <wangtianju0320@sina.com>
2016-05-05 15:14:27 +0800

    Move implementation to .cpp

--- TrainingProject/OPC_Server/OPCServer.cpp
+++ TrainingProject/OPC_Server/OPCServer.cpp
@@ -5,4 +5,34 @@
 
 
 // COPCServer
+STDMETHODIMP COPCServer::AddGroup(LPCWSTR szName, BOOL bActive, DWORD dwRequestedUpdateRate, OPCHANDLE hClientGroup, LONG * pTimeBias, FLOAT * pPercentDeadband, DWORD dwLCID, OPCHANDLE * phServerGroup, DWORD * pRevisedUpdateRate, REFIID riid, LPUNKNOWN * ppUnk)
+{
+    // Add your function implementation here.
+    return E_NOTIMPL;
+}
+STDMETHODIMP COPCServer::GetErrorString(HRESULT dwError, LCID dwLocale, LPWSTR * ppString)
+{
+    // Add your function implementation here.
+    return E_NOTIMPL;
+}
+STDMETHODIMP COPCServer::GetGroupByName(LPCWSTR szName, REFIID riid, LPUNKNOWN * ppUnk)
+{
+    // Add your function implementation here.
+    return E_NOTIMPL;
+}
+STDMETHODIMP COPCServer::GetStatus(OPCSERVERSTATUS ** ppServerStatus)
+{
+    // Add your function implementation here.
+    return E_NOTIMPL;
+}
+STDMETHODIMP COPCServer::RemoveGroup(OPCHANDLE hServerGroup, BOOL bForce)
+{
+    // Add your function implementation here.
+    return E_NOTIMPL;
+}
+STDMETHODIMP COPCServer::CreateGroupEnumerator(OPCENUMSCOPE dwScope, REFIID riid, LPUNKNOWN * ppUnk)
+{
+    // Add your function implementation here.
+    return E_NOTIMPL;
+}
 
